f782708dabafb3973efe338b86a1d9afe1485ee6
alastaira <alastair@playful.technology>
2019-09-04 16:40:58 +0100

    PWM v1.0

--- AnimatronicController_PWM/AnimatronicController_PWM.ino
+++ AnimatronicController_PWM/AnimatronicController_PWM.ino
@@ -1,18 +1,28 @@
-// Arduino pins are grouped into 3 "ports" that share a pin change interrupt
-// Set the PORTS_TO_USE value to specify which group of pins to use to read the RC ahnnel inputs
-// PORTS_TO_USE  1 for pins D8-D13
+/**
+ * Realtime Arduino Animatronic Controller (6 channel, PWM version)
+ * 
+ * This Arduino script uses the "FastRCReader" library to read PWM input channels from a RC receiver such as the Flysky FS-iA6,
+ * It then applies various logic to these inputs based on a chosen animatronic model (tentacle, eye, etc.) and assigns corresponding
+ * positions of an array of servo motors via a PCA9685 PWM controller.
+ */
+
+// DEFINES
+// For efficient, non-blocking reading of the PWM inputs, the FastRCReader uses pin change interrupts
+// Arduino pins are grouped into 3 "ports" that share a pin change interrupt, so you need to set the 
+// following PORTS_TO_USE value to specify which group of pins are used for the RC channel inputs
+// PORTS_TO_USE 1 for pins D8-D13
 // PORTS_TO_USE 2 for pins A0-A5
 // PORTS_TO_USE 3 for pins D0-D7
 #define PORTS_TO_USE 3
 
-//INCLUDES
+// INCLUDES
 // Wire library used for I2C communication
 #include <Wire.h>
 // PCA9685 16-channel PWM controller
-// https://github.com/NachtRaveVL/PCA9685-Arduino
+// See: https://github.com/NachtRaveVL/PCA9685-Arduino
 #include "src/PCA9685/PCA9685.h"
 // Helper library for reading PPM signals from RC receiver
-// https://github.com/timoxd7/FastRCReader
+// See: https://github.com/timoxd7/FastRCReader
 #include "src/FastRCReader/FastRCReader.h"
 
 // CONSTANTS
@@ -20,12 +30,16 @@
 const int numInputChannels = 6;
 // Each channel will be assigned a unique pin in the range defined by PORTS_TO_USE
 uint8_t channelPins[numInputChannels] = {7,6,5,4,3,2};
+// This is the number of output channels (i.e. servo motors) to be controlled by the script
+// Note that the logic applied by the script means that there is not a 1:1 mapping between inputs and outputs
+// so this number can be greater or less than the number of inputs 
 const byte numOutputChannels = 4;
 
+// Define some common animatronic behaviour types
 enum Behaviour {
   SimpleTentacle, // X/Y tentacle movement only e.g. https://www.thingiverse.com/thing:6986
   WigglyTentacle, // X/Y tentacle with random wigggles added.
-  DualTentacles, // X/Y x 2
+  DualTentacles, // 2x independent X/Y movement
   SimpleEye, // X movement of eye, Y movement of eyelids
   Eye,
 };
@@ -36,72 +50,80 @@ PCA9685_ServoEvaluator pwmServo;
 FastRCReader RC;
 Behaviour behaviour = Behaviour::DualTentacles;
 
-// Array of current channel input values
+// Array of input channel values received from FS-iA6
 int channelInput[numInputChannels];
+// Array of output channel values to send to PCA9685
 int channelOutput[numOutputChannels];
 
-// This function processes inputs and clacualted the appropriate outputs.
-// It applies different behaviour for, say, a tentacle or a pair of eyes
+// This function processes inputs and calculates the appropriate outputs
+// based on the selected model
 void ApplyLogic(){
-
+  // Behaviour depends on chosen model
   switch(behaviour) {
-
-    case Behaviour::SimpleTentacle: 
-    {
+    
+    case Behaviour::SimpleTentacle: {
+      // Calculate movement in X axis
       int X = 0;
       // The ON period of almost all RC pulses range from 1000us to 2000us.
       // We'll remap this to an angle from -45 to +45
       if(channelInput[0] > 995 && channelInput[0] < 2000) { 
         X = map(channelInput[0], 1000, 2000, -90, 90);
       }
-      //Serial.println(X);
+      // Calculate PWM corresponding to desired angle and assign to output channel 0
       channelOutput[0] = pwmServo.pwmForAngle(X);
 
+      // Calculate movement in the Y axis
       int Y = 0;
       if(channelInput[1] > 995 && channelInput[1] < 2000) { 
         Y = map(channelInput[1], 1000, 2000, -90, 90);
       }
+      // Calculate PWM corresponding to desired angle and assign to output channel 1
       channelOutput[1] = pwmServo.pwmForAngle(Y);
-    }
+      }
       break;
 
-    case Behaviour::WigglyTentacle: 
-    {
+    case Behaviour::WigglyTentacle: {
+      // Calculate movement in X axis
       int X = 0;
       // The ON period of almost all RC pulses range from 1000us to 2000us.
       // We'll remap this to an angle from -45 to +45
       if(channelInput[0] > 995 && channelInput[0] < 2000) { 
         X = map(channelInput[0], 1000, 2000, -90, 90);
       }
-      float theta = millis() * 0.005;
-      X += 6 * sin(theta);   
+      // Calculate additional periodic wiggly offset
+      X += 6 * sin(millis() * 0.005);
+      // Calculate PWM corresponding to desired angle and assign to output channel 0
       channelOutput[0] = pwmServo.pwmForAngle(X);
 
+      // Calculate movement in Y axis
       int Y = 0;
       if(channelInput[1] > 995 && channelInput[1] < 2000) { 
         Y = map(channelInput[1], 1000, 2000, -90, 90);
       }
-      Y += 5 * cos(theta * 1.01);
+      // Calculate additional periodic wiggly offset
+      Y += 5 * cos(millis() * 0.0051);
+
+      // Calculate PWM corresponding to desired angle and assign to output channel 1
       channelOutput[1] = pwmServo.pwmForAngle(Y);
-    }
+      }
       break;
 
 
-    case Behaviour::DualTentacles: 
-    {
+    case Behaviour::DualTentacles: {
       int X1 = 0;
       // The ON period of almost all RC pulses range from 1000us to 2000us.
-      // We'll remap this to an angle from -45 to +45
+      // We'll remap this to an angle from -90 to +90
       if(channelInput[0] > 995 && channelInput[0] < 2000) { 
         X1 = map(channelInput[0], 1000, 2000, -90, 90);
       }
-      //Serial.println(X);
+      // Calculate PWM corresponding to desired angle and assign to output channel 0
       channelOutput[0] = pwmServo.pwmForAngle(X1);
 
       int Y1 = 0;
       if(channelInput[1] > 995 && channelInput[1] < 2000) { 
         Y1 = map(channelInput[1], 1000, 2000, -90, 90);
       }
+      // Calculate PWM corresponding to desired angle and assign to output channel 1
       channelOutput[1] = pwmServo.pwmForAngle(Y1);
 
       int X2 = 0;
@@ -110,28 +132,27 @@ void ApplyLogic(){
       if(channelInput[0] > 995 && channelInput[2] < 2000) { 
         X2 = map(channelInput[0], 1000, 2000, -90, 90);
       }
-      //Serial.println(X);
+      // Calculate PWM corresponding to desired angle and assign to output channel 2
       channelOutput[2] = pwmServo.pwmForAngle(X2);
 
       int Y2 = 0;
       if(channelInput[1] > 995 && channelInput[3] < 2000) { 
         Y2 = map(channelInput[1], 1000, 2000, -90, 90);
       }
+      // Calculate PWM corresponding to desired angle and assign to output channel 3
       channelOutput[3 ] = pwmServo.pwmForAngle(Y2);
-      
-    }
-    break;
+      }
+      break;
 
-
-    case Behaviour::SimpleEye: 
-    {
+    case Behaviour::SimpleEye: {
       // EYE LEFT/RIGHT
       int eyeX = 0;
       if(channelInput[0] > 995 && channelInput[0] < 2000) { 
         // The ON period of almost all RC pulses range from 1000us to 2000us.
-        // We'll remap this to an angle from -45 to +45
+        // We'll remap this to an angle from -90 to +90
         eyeX = map(channelInput[0], 1000, 2000, -90, 90);
       }
+      // Calculate PWM corresponding to desired angle and assign to output channel 0
       channelOutput[0] = pwmServo.pwmForAngle(eyeX);
 
       // EYELID UP/DOWN
@@ -147,12 +168,12 @@ void ApplyLogic(){
       else if(millis() > nextBlinkTime + 200) {
         nextBlinkTime += random(5000, 8000);
       }
+      // Calculate PWM corresponding to desired angle and assign to output channel 1
       channelOutput[1] = pwmServo.pwmForAngle(eyelidY);
-    }
-    break;
+      }
+      break;
 
-    case Behaviour::Eye: 
-    {
+    case Behaviour::Eye: {
      // The ON period of almost all RC pulses range from 1000us to 2000us.
       // We'll remap this to an angle from -45 to +45
       int eyeX = map(channelInput[0], 1000, 2000, -30, 30);
@@ -165,8 +186,8 @@ void ApplyLogic(){
       channelOutput[2] = pwmServo.pwmForAngle(topEyelid);
       int bottomEyelid = eyesWideOpen - eyeY / 2;
       channelOutput[3] = pwmServo.pwmForAngle(bottomEyelid);
-    }
-    break;
+      }
+      break;
     
     default: 
       break;
@@ -175,24 +196,22 @@ void ApplyLogic(){
 
 
 void setup() {
-  // Initialise the serial connection
+  // Initialise the serial connection (used for monitoring for Arduino IDE Serial Monitor)
   Serial.begin(9600);
 
+  // Start the interface for the RC receiver
   RC.begin();
   RC.addChannel(channelPins, numInputChannels);
 
-  // Initialise I2C interface used 
+  // Initialise I2C interface used for the PCA9685 PWM controller
   Wire.begin();
   // Supported baud rates are 100kHz, 400kHz, and 1000kHz
   Wire.setClock(400000);
-
-    pwmController.resetDevices();       // Software resets all PCA9685 devices on Wire line
-
-    pwmController.init();        // Address pins A5-A0 set to B000000
-    pwmController.setPWMFrequency(50);  // 50Hz provides 20ms standard servo phase length
-
-//    pwmController.setChannelPWM(0, 128 << 4); // Set PWM to 128/255, but in 4096 land
-
+  // Initialise PCA9685
+  pwmController.resetDevices();
+  pwmController.init();
+  // 50Hz provides 20ms standard servo phase length
+  pwmController.setPWMFrequency(50);  
 }
 
 void loop() {
@@ -203,11 +222,12 @@ void loop() {
       Serial.print(",");
   }
   Serial.println();
-
   
-  // Now perform whatever logic is necessary
+  // Now perform whatever logic is necessary to convert from the array of input channels
+  // into the array of output values
   ApplyLogic();
-  
+
+  // Now, send the output values to the PWM servo controller
   for(int i=0; i<numOutputChannels; i++) {
     pwmController.setChannelPWM(i, channelOutput[i]);
   }
